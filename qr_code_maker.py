22c44006a4d40d4c4816bdb1a7f65d783fda8387
mike.gibbens@siemens.com <mike.gibbens@siemens.com>
2024-05-08 18:14:25 +0100

    Util to make QR codes so the kids can access files on their phones easily.

---
+++ qr_code_maker.py
@@ -0,0 +1,20 @@
+import qrcode
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+# Add data to the QR code
+data = "https://github.com/DrMikeG/QrImageMaker"
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("QR_Code_01.png")
